e858a57c45876d52febd4db917f46f8928942f02
Joy <lmot_2005@yahoo.com>
2025-05-28 15:46:16 -0700

    add nano  to linux-onboarding.sh

--- onboarding-linux.sh
+++ onboarding-linux.sh
@@ -1,3 +1,12 @@
+# nano should be installed
+if (which nano)
+then
+  echo 'nano is already installed' 
+else
+  echo 'installing nano'
+  sudo apt install -y nano
+fi
+
 # git should be installed
 if (git --version)
 then
