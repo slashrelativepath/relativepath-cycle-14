0e3990f658be8d5c73e3ca79dbdaad193fc84f18
William McQuaid <whmcquaid@gmail.com>
2025-06-25 15:59:34 -0700

    Change user and path in unit file.

--- node.sh
+++ node.sh
@@ -17,10 +17,29 @@ else
 fi
 
 # app.service should be deployed
-if (stat /etc/systemd/system/app.service)
+if (diff /etc/systemd/system/app.service /home/ubuntu/app.service)
 then
   echo "app.service is already deployed."
 else
   echo "deploying app service..."
   sudo cp /home/ubuntu/app.service /etc/systemd/system/app.service
+  sudo systemctl daemon-reload
+fi
+
+# app.service should be enabled
+if (systemctl is-enabled app.service)
+then
+  echo "app.service is already enabled."
+else
+  echo "enabling app.service..."
+  sudo systemctl enable app.service
+fi
+
+# app.service should be running (active)
+if (systemctl is-active app.service)
+then
+  echo "app.service is already running (active)."
+else
+  echo "starting app.service..."
+  sudo systemctl restart app.service
 fi
